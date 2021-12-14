dc01ec157e7973a278ded1ebb78390580cc06e93
saci zakaria <sacideveloppeur@gmail.com>
2021-12-14 21:50:35 +0100

    final / UserModel()

--- lib/users_screen.dart
+++ lib/users_screen.dart
@@ -4,9 +4,16 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
+class UserModel {
+  final int id;
+  final String name;
+  final String phone;
 
-class UserModel{
-
+  UserModel({
+    this.id,
+    this.name,
+    this.phone
+  });
 }
 
 class UsersScreen extends StatelessWidget {
@@ -19,54 +26,54 @@ class UsersScreen extends StatelessWidget {
         title: Text("Users"),
       ),
       body: ListView.separated(
-          itemBuilder: (context, index) => buildUserItem () ,
-          separatorBuilder:  (context, index) => Padding(
-            padding: const EdgeInsetsDirectional.only(start: 20.0),
-            child: Container(
-              width: double.infinity,
-              height: 1.5,
-              color: Colors.grey[800],
-
-            ),
-          ) ,
+          itemBuilder: (context, index) => buildUserItem(),
+          separatorBuilder: (context, index) => Padding(
+                padding: const EdgeInsetsDirectional.only(start: 20.0),
+                child: Container(
+                  width: double.infinity,
+                  height: 1.5,
+                  color: Colors.grey[800],
+                ),
+              ),
           itemCount: 10),
     );
   }
 
-
-  Widget buildUserItem () => Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: Row(
-      children: [
-        CircleAvatar(
-            radius: 25,
-            child: Text("1",
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold))),
-        SizedBox(width: 20),
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
+  Widget buildUserItem() => Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Row(
           children: [
-            Text("SACI Zakaria",
-              style: TextStyle(
-                  fontSize: 25,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
+            CircleAvatar(
+                radius: 25,
+                child: Text("1",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold))),
+            SizedBox(width: 20),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "SACI Zakaria",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "06.95.06.54.54 ",
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
             ),
-            Text("06.95.06.54.54 ",
-              style: TextStyle(
-                  fontSize: 15,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold),
-            )
           ],
         ),
-      ],
-    ),
-  );
+      );
 // 1. build Item
 // 2. build list
 // 3. add Item to List
