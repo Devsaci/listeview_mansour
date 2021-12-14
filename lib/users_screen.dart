4d6f78e8fa5f487b632fe59b982a68f0dd03da8b
saci zakaria <sacideveloppeur@gmail.com>
2021-12-14 21:17:14 +0100

    Row / CircleAvatar

--- lib/users_screen.dart
+++ lib/users_screen.dart
@@ -1,14 +1,34 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'dart:html';
+import 'dart:ui';
 
-// ignore_for_file: prefer_const_constructors_in_immutables
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UsersScreen extends StatelessWidget {
-   UsersScreen({Key key}) : super(key: key);
+  UsersScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Users"),
+      ),
+      body: Row(
+        children: [
+          CircleAvatar(
+            radius: 25,
+            child: Text("1", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),),
+          ),
+
+        ],
+      ),
+    );
   }
+
+// 1. build Item
+// 2. build list
+// 3. add Item to List
+
 }
