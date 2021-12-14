b09aef5c86f931200a646e2cad0679731ce991d4
saci zakaria <sacideveloppeur@gmail.com>
2021-12-14 21:34:55 +0100

    buildUserItem

--- lib/users_screen.dart
+++ lib/users_screen.dart
@@ -13,42 +13,44 @@ class UsersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Users"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
-          children: [
-            CircleAvatar(
-                radius: 25,
-                child: Text("1",
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold))),
-            SizedBox(width: 20),
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("SACI Zakaria",
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold),
-                ),
-                Text("06.95.06.54.54 ",
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: null,
     );
   }
 
+
+  Widget buildUserItem () => Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Row(
+      children: [
+        CircleAvatar(
+            radius: 25,
+            child: Text("1",
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold))),
+        SizedBox(width: 20),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("SACI Zakaria",
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text("06.95.06.54.54 ",
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ],
+    ),
+  );
 // 1. build Item
 // 2. build list
 // 3. add Item to List
