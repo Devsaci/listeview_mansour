cb05922a87bdc081f1778311754be6910fb96bf4
saci zakaria <sacideveloppeur@gmail.com>
2021-12-14 21:04:21 +0100

    UsersScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messengerdesign/users_screen.dart';
 import 'messager_screen.dart';
 
 void main() => runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner : false,
       title: 'list View Mansour',
-      home: MessengerScreen(),
+      home: UsersScreen(),
     );
   }
 }
---
+++ lib/users_screen.dart
@@ -0,0 +1,14 @@
+
+
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class UsersScreen extends StatelessWidget {
+   UsersScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
